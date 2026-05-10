56e886cd14c5070efd3468934c9b80efc8f3dc10
Piyumal Madhuwantha <121705632+piyumal2105@users.noreply.github.com>
2026-05-10 10:35:51 +0530

    Add vite-plugin-sitemap to dependencies and configure in Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
@@ -9,6 +10,9 @@ export default defineConfig({
     // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
+    sitemap({
+      hostname: 'https://www.ceylongoldenspices.com'
+    })
   ],
   resolve: {
     alias: {
